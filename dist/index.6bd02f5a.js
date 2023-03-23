d19b4afda3ebebef86b74ca297ffedff314e4ffa
Raveena Kholia <raveena.kholia.ext@unit4.com>
2023-03-23 15:51:41 +0530

    new files added

---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,23 @@
+//React.createElement(Object)  ==> HTML(Browser rendering)
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, React.createElement("h1", {
+        id: "heading"
+    }, "Complex structure")),
+    React.createElement("div", {
+        id: "child"
+    }, React.createElement("h1", {
+        id: "heading"
+    }, "Complex structure"))
+]);
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello World from React");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
